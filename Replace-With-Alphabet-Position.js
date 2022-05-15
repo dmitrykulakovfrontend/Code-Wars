f6df2b35d42890c3fff4941e4e5ebb251d341292
Dmitry Kulakov <36533957+dmitrykulakovfrontend@users.noreply.github.com>
2022-05-15 08:38:39 +0300

    Completed Replace With Alphabet Position

--- Replace-With-Alphabet-Position.js
+++ Replace-With-Alphabet-Position.js
@@ -7,7 +7,8 @@ If anything in the text isn't a letter, ignore it and don't return it.
 
 Example
 alphabetPosition("The sunset sets at twelve o' clock.")
-Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string ) */
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string ) 
+*/
 
 // Answer
 
