3f1b8cc1b2cef77217174dbf2b4ad800e58ff3c9
Dmitry Kulakov <36533957+dmitrykulakovfrontend@users.noreply.github.com>
2022-07-20 11:16:58 +0300

    Completed Pete, the baker

---
+++ Pete-the-baker.js
@@ -0,0 +1,44 @@
+/*
+  Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths.
+  Can you help him to find out, how many cakes he could bake considering his recipes?
+
+  Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object)
+  and returns the maximum number of cakes Pete can bake (integer).
+  For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200).
+  Ingredients that are not present in the objects, can be considered as 0.
+
+  Examples:
+
+  // must return 2
+  cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+  // must return 0
+  cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
+
+*/
+
+// Answer:
+
+function cakes(recipe, available) {
+  let cakes = 0;
+  for (ingredient in recipe) {
+    if (!available[ingredient]) {
+      return cakes;
+    }
+  }
+  while (canICookCake(recipe, available)) {
+    cakes += 1;
+    for (ingredient in recipe) {
+      available[ingredient] -= recipe[ingredient];
+    }
+  }
+  return cakes;
+}
+
+function canICookCake(recipe, available) {
+  for (ingredient in recipe) {
+    if (available[ingredient] < recipe[ingredient]) return false;
+  }
+  return true;
+}
+
+// BigO: O(n2)
