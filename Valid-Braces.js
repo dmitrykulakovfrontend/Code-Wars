f717e2469e60176a96b0df8628e8277e1fe0ba12
Dmitry Kulakov <atomeistee@gmail.com>
2022-09-09 17:04:16 +0300

    Completed Valid Braces

---
+++ Valid-Braces.js
@@ -0,0 +1,43 @@
+/*
+  Write a function that takes a string of braces, and determines if the order of the braces is valid.
+  It should return true if the string is valid, and false if it's invalid.
+
+  This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+  All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+  What is considered Valid?
+  A string of braces is considered valid if all braces are matched with the correct brace.
+
+  Examples
+  "(){}[]"   =>  True
+  "([{}])"   =>  True
+  "(}"       =>  False
+  "[(])"     =>  False
+  "[({})](]" =>  False
+*/
+
+// Answer:
+
+const pair = {
+  ']': '[',
+  '}': '{',
+  ')': '(',
+}
+function validBraces(braces){
+  const stack = []
+  for (const brace of braces) {
+    if (Object.values(pair).includes(brace)) {
+      stack.push(brace)
+    } else {
+      if (stack.length === 0) return false
+      let openingBrace = stack.pop();
+      if (pair[brace] !== openingBrace) {
+        return false
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+// BigO: O(n)
