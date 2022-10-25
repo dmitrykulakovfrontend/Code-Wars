0d2094420d0a4330a902547227eceb9df698c026
Dmitry Kulakov <atomeistee@gmail.com>
2022-10-25 10:24:52 +0300

    completed Strip Comments

---
+++ Strip-Comments.js
@@ -0,0 +1,44 @@
+/*
+  Complete the solution so that it strips all text that follows any of a set of comment markers passed in.
+  Any whitespace at the end of the line should also be stripped out.
+
+  Example:
+
+  Given an input string of:
+
+  apples, pears # and bananas
+  grapes
+  bananas !apples
+  The output expected would be:
+
+  apples, pears
+  grapes
+  bananas
+  The code would be called like so:
+
+  var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+  // result should == "apples, pears\ngrapes\nbananas"
+*/
+
+// Answer:
+
+function solution(input, markers) {
+  let result = '';
+  let lines = input.split('\n');
+  
+  lines.forEach(line => {
+    for (let char of line) {
+      if (markers.includes(char)) {
+        break;
+      } else {
+        result += char;
+      }
+    }
+    
+    result = result.trim();
+    result += '\n';
+  })
+  return result.trim()
+};
+
+// BigO: O(n2)
