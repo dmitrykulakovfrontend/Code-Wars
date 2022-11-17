60094857afbde75c5b43976489d4dabedfc56759
Dmitry Kulakov <atomeistee@gmail.com>
2022-11-17 19:49:30 +0300

    completed Multiply Word in String

---
+++ Multiply-Word-in-String.ts
@@ -0,0 +1,21 @@
+/*
+  You are to write a function that takes a string as its first parameter. This string will be a string of words.
+
+  You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string.
+  The first word would be represented by 0.
+
+  Once you have the located string you are finally going to multiply by it the third provided parameter, which will also be an integer.
+  You are additionally required to add a hyphen in between each word.
+
+  Example
+
+  modifyMultiply ("This is a string",3,5) 
+*/
+
+// Answer:
+
+function modifyMultiply (str: string, loc: number, num: number): string {
+  return (str.split(' ')[loc] + '-').repeat(num).slice(0,-1)
+} 
+
+// BigO: O(n)
