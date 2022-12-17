698e9af7e9d86020c29f086da76ac46bf8ec105d
Dmitry Kulakov <atomeistee@gmail.com>
2022-12-17 11:59:25 +0300

    completed Bumps in the Road

---
+++ Bumps-in-the-Road.js
@@ -0,0 +1,16 @@
+/*
+  Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+  Unfortunately for you, your drive is very bumpy!
+  Given a string showing either flat road (_) or bumps (n).
+  If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+*/
+
+// Answer:
+
+function bump(x){
+  let bumps =x.split('').reduce((acc,curr) => curr === 'n' ? acc += 1 : acc,0);
+  return bumps > 15 ? "Car Dead" : "Woohoo!"
+}
+
+// BigO: O(n)
