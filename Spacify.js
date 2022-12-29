906c07da028737a8d0f34ed2cfcf75a4ffda3825
Dmitry Kulakov <atomeistee@gmail.com>
2022-12-29 08:00:00 +0300

    completed Spacify

---
+++ Spacify.js
@@ -0,0 +1,13 @@
+/*
+  Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+  spacify("hello world") // returns "h e l l o   w o r l d"
+*/
+
+// Answer:
+
+function spacify(str) {
+  return str.split("").join(" ");
+}
+
+// BigO: O(n)
