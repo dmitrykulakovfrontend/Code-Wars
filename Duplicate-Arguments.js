45d2d8b242d659d51642b43c056f69bf15f54ee5
Dmitry Kulakov <atomeistee@gmail.com>
2023-01-23 15:11:37 +0300

    completed Duplicate Arguments

---
+++ Duplicate-Arguments.js
@@ -0,0 +1,24 @@
+/*
+  Complete the solution so that it returns true if it contains any duplicate argument values. Any number of arguments may be passed into the function.
+
+  The array values passed in will only be strings or numbers. The only valid return values are true and false.
+
+  Examples:
+
+  solution(1, 2, 3)             -->  false
+  solution(1, 2, 3, 2)          -->  true
+  solution('1', '2', '3', '2')  -->  true
+*/
+
+// Answer:
+
+function solution(...args){
+ const result = new Map();
+  for (const arg of args) {
+    if (result.has(arg)) return true;
+    result.set(arg,true);
+  }
+  return false;
+}
+
+// BigO: O(n)
