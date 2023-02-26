fac41703cf67495ca2abc1ee3d7576cac455f9bb
Dmitry Kulakov <atomeistee@gmail.com>
2023-02-26 22:47:31 +0300

    completed Get number from string

---
+++ Get-number-from-string.js
@@ -0,0 +1,14 @@
+/*
+  Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+  Function:
+
+  getNumberFromString(s)
+*/
+
+// Answer:
+function getNumberFromString(s) {
+  return Number(s.match(/\d+/g).join(""));
+}
+
+// BigO: O(n)
