3ed4d053fb186e2cf31345fa137cb87615b8c616
Dmitry Kulakov <atomeistee@gmail.com>
2023-04-18 20:00:00 +0300

    completed Between Extremes

---
+++ Between_Extremes.java
@@ -0,0 +1,29 @@
+/*
+    Given an array of numbers, return the difference between the largest and smallest values.
+
+  For example:
+
+  [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+  [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+  The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+*/
+
+// Answer:
+
+public class Between_Extremes {
+  public static int betweenExtremes(int[] numbers) {
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
+    for (int num : numbers) {
+      if (num > max)
+        max = num;
+      if (num < min)
+        min = num;
+    }
+    return max - min;
+  }
+}
+
+// BigO: O(n)
