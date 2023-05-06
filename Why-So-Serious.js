462fc02d524e6b1d4394b02c91ad4c75e5d14a59
Dmitry Kulakov <atomeistee@gmail.com>
2023-05-06 20:34:32 +0300

    completed Why So Serious?

---
+++ Why-So-Serious.js
@@ -0,0 +1,27 @@
+/*
+  "Introduce a little anarchy. Upset the established order and everything becomes chaos…" – Joker
+
+  "The city needs you, Batman! The Kings and Queens are being held for ransom and our only lead is a package we just received from the Joker: half a pack of cards, a jumble of numbers and letters, and a smile emoji. We have an hour... can you find him?" – Gotham City PD
+
+  Task
+  Help Gotham City PD decode the puzzle below. If all the letters of the Joker's name are encoded in the string of cards, return true. If not, return false. Note the input is a string of shuffled cards i.e. "6s5s4cAs4s2c7s..." and the name can be found in any order.
+
+  Card-letter correspondance
+   Card | Letter
+  ------+--------
+    Jc  |   J
+    7s  |   O
+    5s  |   K
+    As  |   E
+    9c  |   R
+  Examples
+  "10s8s9c2s5s7c2c9s7sJc5cAs"  =>  true
+  "9s4s8c6s4c10c8sJs3s"        =>  false
+*/
+
+// Answer:
+function whySoSerious(str) {
+  return ['Jc','7s','5s','As','9c'].every(k=>str.includes(k));
+}
+
+// BigO: O(n)
